0585fcbbea3c09e981d3c98ef6b10ebc636d16f1
Lucas Sadzik <ritsuboshi@gmail.com>
2020-05-09 05:39:06 +0200

    feat: Create an endpoint to GET drink details

--- backend/src/api.py
+++ backend/src/api.py
@@ -45,7 +45,19 @@ def get_drinks():
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks-detail')
+@requires_auth('get:drinks-detail')
+def get_drinks_detail(jwt):
+    try:
+        drinks = Drink.query.all()
+        formatted_drinks = [drink.long() for drink in drinks]
 
+        return jsonify({
+            'success': True,
+            'drinks': formatted_drinks
+        }), 200
+    except Exception:
+        abort(422)
 
 '''
 @TODO implement endpoint
