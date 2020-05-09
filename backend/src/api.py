93d85a41f302ae62a49dd04d8610d85fae4472c4
Lucas Sadzik <ritsuboshi@gmail.com>
2020-05-09 08:57:12 +0200

    feat: Implement error handlers for expected errors

--- backend/src/api.py
+++ backend/src/api.py
@@ -149,36 +149,58 @@ def delete_drink(jwt, drink_id):
     except Exception:
         abort(422)
 
+
 ## Error Handling
-'''
-Example error handling for unprocessable entity
-'''
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        'success': False,
+        'error': 400,
+        'message': 'bad request'
+    }), 400
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 404,
+        'message': 'resource not found'
+    }), 404
+
+
+@app.errorhandler(405)
+def not_allowed(error):
+    return jsonify({
+        'success': False,
+        'error': 405,
+        'message': 'method not allowed'
+    }), 405
+
+
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
-                    "success": False,
-                    "error": 422,
-                    "message": "unprocessable"
-                    }), 422
-
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False,
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
-'''
-
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above
-'''
+        'success': False,
+        'error': 422,
+        'message': 'unprocessable'
+    }), 422
 
 
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above
-'''
+@app.errorhandler(500)
+def server_error(error):
+    return jsonify({
+        'success': False,
+        'error': 500,
+        'message': 'internal server error'
+    }), 500
+
+
+@app.errorhandler(AuthError)
+def handle_auth_error(error):
+    return jsonify({
+        'success': False,
+        'error': error.status_code,
+        'message': error.error
+    }), error.status_code
